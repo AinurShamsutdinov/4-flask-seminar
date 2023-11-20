51f1f32d7b7a2835d0c7546c260f4301caeaecbc
Ainur Shamsutdinov <shamsut@gmail.com>
2023-11-20 22:42:17 +0300

    6th exercise solution

--- app_04.py
+++ app_04.py
@@ -3,19 +3,18 @@ import time
 from os import listdir
 from os.path import isfile, join
 
+mypath = '/Users/ainur/projects/PycharmProjects/4-flask-seminar/'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+threads = []
+
 
 def count_words(filename):
-    print(f'filename {filename}')
     file = open(mypath + filename, 'rt')
     data = file.read()
     words_count = len(data.split())
     print(f'filename {filename}, words count {words_count}')
 
 
-mypath = '/Users/ainur/projects/PycharmProjects/4-flask-seminar/'
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-threads = []
-start_time = time.time()
 for filename in onlyfiles:
     thread = threading.Thread(target=count_words, args=[filename])
     threads.append(thread)
--- app_06.py
+++ app_06.py
@@ -1,25 +1,29 @@
-import threading
-import time
-from multiprocessing import Process
+import asyncio
 from os import listdir
 from os.path import isfile, join
+import time
 
 mypath = '/Users/ainur/projects/PycharmProjects/4-flask-seminar/'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-processes = []
 
 
-def count_words(filename):
+async def count_words(filename):
+    print(f'filename {filename}')
     file = open(mypath + '/' + filename, 'rt')
     data = file.read()
     words_count = len(data.split())
     print(f'filename {filename}, words count {words_count}')
 
 
-if __name__ == '__main__':
+async def main():
+    tasks = []
     for filename in onlyfiles:
-        process = Process(target=count_words, args=(filename,))
-        processes.append(process)
-        process.start()
-    for process in processes:
-        process.join()
+        task = asyncio.ensure_future(count_words(filename))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
+
+start_time = time.time()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
